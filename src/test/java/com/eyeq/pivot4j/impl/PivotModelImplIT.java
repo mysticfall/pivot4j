20877c0064c0190a886848d0679bd125988d79c3
Xavier Cho <fender@eyeq.co.kr>
2012-09-21 18:14:05 +0900

    Added a log4j configuration file for unit testing

--- src/test/java/com/eyeq/pivot4j/impl/PivotModelImplIT.java
+++ src/test/java/com/eyeq/pivot4j/impl/PivotModelImplIT.java
@@ -8,11 +8,16 @@
  */
 package com.eyeq.pivot4j.impl;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Test;
+import org.olap4j.CellSet;
+import org.olap4j.CellSetAxis;
 
 import com.eyeq.pivot4j.AbstractIntegrationTestCase;
 import com.eyeq.pivot4j.NotInitializedException;
@@ -21,39 +26,72 @@ import com.eyeq.pivot4j.PivotModel;
 
 public class PivotModelImplIT extends AbstractIntegrationTestCase {
 
-	@Test(expected = PivotException.class)
-	public void testNoInitialMDX() {
-		PivotModel model = getPivotModel();
+	private String testQuery = "select {[Measures].[Unit Sales], [Measures].[Store Cost], [Measures].[Store Sales]} ON COLUMNS, "
+			+ "{([Promotion Media].[All Media], [Product].[All Products])} ON ROWS from [Sales] where [Time].[1997]";
 
-		assertNotNull(model);
-
-		assertFalse(model.isInitialized());
-		model.initialize();
+	/**
+	 * @return the testQuery
+	 */
+	public String getTestQuery() {
+		return testQuery;
 	}
 
-	@Test(expected = NotInitializedException.class)
-	public void testOperationBeforeInitialize() {
-		PivotModel model = getPivotModel();
-
-		assertNotNull(model);
-
-		assertFalse(model.isInitialized());
-
-		model.getCellSet();
+	/**
+	 * @param testQuery
+	 *            the testQuery to set
+	 */
+	public void setTestQuery(String testQuery) {
+		this.testQuery = testQuery;
 	}
 
 	@Test
 	public void testInitialize() {
 		PivotModel model = getPivotModel();
+		model.setMdx(getTestQuery());
 
-		assertNotNull(model);
-
-		model.setMdx("select {[Measures].[Unit Sales], [Measures].[Store Cost], [Measures].[Store Sales]} ON COLUMNS, "
-				+ "{([Promotion Media].[All Media], [Product].[All Products])} ON ROWS from [Sales] where [Time].[1997]");
-
-		assertFalse(model.isInitialized());
+		assertFalse("Model is already initialized.", model.isInitialized());
 		model.initialize();
 
-		assertTrue(model.isInitialized());
+		assertTrue("Model is not initialized.", model.isInitialized());
+	}
+
+	@Test(expected = PivotException.class)
+	public void testInitializeWithNoInitialMDX() {
+		PivotModel model = getPivotModel();
+		model.initialize();
+	}
+
+	@Test
+	public void testDestroy() {
+		PivotModel model = getPivotModel();
+		model.setMdx(getTestQuery());
+		model.initialize();
+
+		assertTrue("Model is not initialized.", model.isInitialized());
+
+		model.destroy();
+		assertFalse("Model is not destroyed.", model.isInitialized());
+	}
+
+	@Test
+	public void testGetCellSet() {
+		PivotModel model = getPivotModel();
+		model.setMdx(getTestQuery());
+		model.initialize();
+
+		CellSet cellSet = model.getCellSet();
+		assertNotNull("CellSet is null.", cellSet);
+
+		List<CellSetAxis> axes = cellSet.getAxes();
+		assertNotNull("Cell axes list is null.", axes);
+		assertEquals("Invalid cell axes size.", 2, axes.size());
+	}
+
+	@Test(expected = NotInitializedException.class)
+	public void testGetCellSetBeforeInitialize() {
+		PivotModel model = getPivotModel();
+		assertFalse("Model is already initialized.", model.isInitialized());
+
+		model.getCellSet();
 	}
 }
