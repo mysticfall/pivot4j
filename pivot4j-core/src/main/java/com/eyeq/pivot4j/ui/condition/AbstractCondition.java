658ba5c7a1d76d71a6f40f697134a423a3423f4c
Xavier Cho <fender@eyeq.co.kr>
2013-04-26 23:18:01 +0900

    #67: Implement conditional formatting API
    Add basic logical conditions.

---
+++ pivot4j-core/src/main/java/com/eyeq/pivot4j/ui/condition/AbstractCondition.java
@@ -0,0 +1,34 @@
+/*
+ * ====================================================================
+ * This software is subject to the terms of the Common Public License
+ * Agreement, available at the following URL:
+ *   http://www.opensource.org/licenses/cpl.html .
+ * You must accept the terms of that agreement to use this software.
+ * ====================================================================
+ */
+package com.eyeq.pivot4j.ui.condition;
+
+import org.apache.commons.lang.NullArgumentException;
+
+public abstract class AbstractCondition implements Condition {
+
+	private ConditionFactory conditionFactory;
+
+	/**
+	 * @param conditionFactory
+	 */
+	public AbstractCondition(ConditionFactory conditionFactory) {
+		if (conditionFactory == null) {
+			throw new NullArgumentException("conditionFactory");
+		}
+
+		this.conditionFactory = conditionFactory;
+	}
+
+	/**
+	 * @return the conditionFactory
+	 */
+	public ConditionFactory getConditionFactory() {
+		return conditionFactory;
+	}
+}
