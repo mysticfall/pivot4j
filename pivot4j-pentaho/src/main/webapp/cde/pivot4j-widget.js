e5df16207463e8bca41b7a4fc184a60e71ddd1b9
Xavier Cho <fender@eyeq.co.kr>
2014-04-06 18:39:51 +0900

    Fix #141: Integrate Pivot4J to a CDE Dashboard in Pentaho
    http://github.com/mysticfall/pivot4j/issues/issue/141

---
+++ pivot4j-pentaho/src/main/webapp/cde/pivot4j-widget.js
@@ -0,0 +1,28 @@
+var Pivot4JComponent = BaseComponent.extend({
+
+	update : function() {
+		if (!this.htmlObject) {
+			return;
+		}
+
+		var widget = $("#" + this.htmlObject);
+		var iframe = $(document.createElement("iframe"));
+
+		if (this.width) {
+			iframe.width(this.width);
+		}
+
+		if (this.width) {
+			iframe.height(this.height);
+		}
+
+		var path = encodeURIComponent(this.filePath);
+
+		iframe.attr("frameBorder", "none").attr(
+				"src",
+				"../../../plugin/pivot4j/faces/open.xhtml?embeded=true&path="
+						+ path);
+
+		widget.append(iframe);
+	}
+});
