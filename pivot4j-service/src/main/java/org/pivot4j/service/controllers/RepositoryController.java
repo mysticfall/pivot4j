997fc7e0a8e8704f69ce2a6b7ae8b23e18935170
Xavier Cho <mysticfallband@gmail.com>
2014-09-14 17:44:03 +0900

    Add license notification

--- pivot4j-service/src/main/java/org/pivot4j/service/controllers/RepositoryController.java
+++ pivot4j-service/src/main/java/org/pivot4j/service/controllers/RepositoryController.java
@@ -1,3 +1,10 @@
+/*
+ * This software is subject to the terms of the Common Public License
+ * Agreement, available at the following URL:
+ *   http://www.opensource.org/licenses/cpl.html .
+ * You must accept the terms of that agreement to use this software.
+ * ====================================================================
+ */
 package org.pivot4j.service.controllers;
 
 import java.io.IOException;
