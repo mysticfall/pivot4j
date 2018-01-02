6f16265e5acc40063d4019fae031ab696d7994fe
Judilson José da Costa Júnior <judilsonjunior@gmail.com>
2018-01-02 12:08:13 -0200

    Merge master and primefaces 6.1

--- pivot4j-core/src/main/java/org/pivot4j/transform/impl/DrillThroughImpl.java
+++ pivot4j-core/src/main/java/org/pivot4j/transform/impl/DrillThroughImpl.java
@@ -14,6 +14,7 @@ import java.sql.Statement;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import javax.servlet.ServletRequest;
 
 import org.apache.commons.lang.NullArgumentException;
 import org.olap4j.Cell;
@@ -38,173 +39,175 @@ import org.slf4j.LoggerFactory;
 
 public class DrillThroughImpl extends AbstractTransform implements DrillThrough {
 
-	private Logger logger = LoggerFactory.getLogger(getClass());
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
-	/**
-	 * @param queryAdapter
-	 * @param connection
-	 */
-	public DrillThroughImpl(QueryAdapter queryAdapter, OlapConnection connection) {
-		super(queryAdapter, connection);
-	}
+    /**
+     * @param queryAdapter
+     * @param connection
+     */
+    public DrillThroughImpl(QueryAdapter queryAdapter, OlapConnection connection) {
+        super(queryAdapter, connection);
+    }
 
-	/**
-	 * @return the logger
-	 */
-	protected Logger getLogger() {
-		return logger;
-	}
+    /**
+     * @return the logger
+     */
+    protected Logger getLogger() {
+        return logger;
+    }
 
-	/**
-	 * @see org.pivot4j.transform.DrillThrough#drillThrough(org.olap4j.Cell)
-	 */
-	@Override
-	public ResultSet drillThrough(Cell cell) {
-		return drillThrough(cell, null, 0);
-	}
+    /**
+     * @see org.pivot4j.transform.DrillThrough#drillThrough(org.olap4j.Cell)
+     */
+    @Override
+    public ResultSet drillThrough(Cell cell) {
+        return drillThrough(cell, null, 0);
+    }
 
-	/**
-	 * @see org.pivot4j.transform.DrillThrough#drillThrough(org.olap4j.Cell,
-	 *      java.util.List, int)
-	 */
-	@Override
-	public ResultSet drillThrough(Cell cell, List<MetadataElement> selection,
-			int maximumRows) {
-		if (cell == null) {
-			throw new NullArgumentException("cell");
-		}
+    /**
+     * @see org.pivot4j.transform.DrillThrough#drillThrough(org.olap4j.Cell,
+     * java.util.List, int)
+     */
+    @Override
+    public ResultSet drillThrough(Cell cell, List<MetadataElement> selection,
+            int maximumRows) {
+        if (cell == null) {
+            throw new NullArgumentException("cell");
+        }
 
-		ResultSet result;
+        ResultSet result;
 
-		if (selection==null) {
-			selection = Collections.emptyList();
-		}
+        if (selection == null) {
+            selection = Collections.emptyList();
+        }
 
-		result = performDrillThroughMdx(cell, selection, maximumRows);
+        result = performDrillThroughMdx(cell, selection, maximumRows);
 
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * @param cell
-	 * @return
-	 */
-	protected ResultSet performDrillThrough(Cell cell) {
-		try {
-			return cell.drillThrough();
-		} catch (OlapException e) {
-			throw new PivotException(e);
-		}
-	}
+    /**
+     * @param cell
+     * @return
+     */
+    protected ResultSet performDrillThrough(Cell cell) {
+        try {
+            return cell.drillThrough();
+        } catch (OlapException e) {
+            throw new PivotException(e);
+        }
+    }
 
-	/**
-	 * @param cell
-	 * @param selection
-	 * @param maximumRows
-	 * @return
-	 */
-	protected ResultSet performDrillThroughMdx(Cell cell,
-			List<MetadataElement> selection, int maximumRows) {
-		PivotModel model = getModel();
-		Cube cube = model.getCube();
+    /**
+     * @param cell
+     * @param selection
+     * @param maximumRows
+     * @return
+     */
+    private ServletRequest session;
+    
+    protected ResultSet performDrillThroughMdx(Cell cell,
+            List<MetadataElement> selection, int maximumRows) {
+        PivotModel model = getModel();
+        Cube cube = model.getCube();
 
-		QueryAdapter query = getQueryAdapter();
+        QueryAdapter query = getQueryAdapter();
 
-		StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
 
-		builder.append("DRILLTHROUGH");
+        builder.append("DRILLTHROUGH");
 
-		if (maximumRows > 0) {
-			builder.append(" MAXROWS ");
-			builder.append(maximumRows);
-		}
+        if (maximumRows > 0) {
+            builder.append(" MAXROWS ");
+            builder.append(maximumRows);
+        }
 
-		builder.append(" SELECT (");
+        builder.append(" SELECT (");
 
-		boolean isFirst = true;
+        boolean isFirst = true;
 
-		List<Integer> coords = cell.getCoordinateList();
+        List<Integer> coords = cell.getCoordinateList();
 
-		CellSet cellSet = cell.getCellSet();
-		List<CellSetAxis> axes = cellSet.getAxes();
+        CellSet cellSet = cell.getCellSet();
+        List<CellSetAxis> axes = cellSet.getAxes();
 
-		OlapUtils utils = new OlapUtils(getModel().getCube());
-		utils.setMemberHierarchyCache(getQueryAdapter().getModel()
-				.getMemberHierarchyCache());
+        OlapUtils utils = new OlapUtils(getModel().getCube());
+        utils.setMemberHierarchyCache(getQueryAdapter().getModel()
+                .getMemberHierarchyCache());
 
-		int axisOrdinal = 0;
-		for (int ordinal : coords) {
-			Position position = axes.get(axisOrdinal++).getPositions()
-					.get(ordinal);
+        int axisOrdinal = 0;
+        for (int ordinal : coords) {
+            Position position = axes.get(axisOrdinal++).getPositions()
+                    .get(ordinal);
 
-			for (Member member : position.getMembers()) {
-				if (isFirst) {
-					isFirst = false;
-				} else {
-					builder.append(", ");
-				}
+            for (Member member : position.getMembers()) {
+                if (isFirst) {
+                    isFirst = false;
+                } else {
+                    builder.append(", ");
+                }
 
-				builder.append(utils.wrapRaggedIfNecessary(member)
-						.getUniqueName());
-			}
-		}
+                builder.append(utils.wrapRaggedIfNecessary(member)
+                        .getUniqueName());
+            }
+        }
 
-		builder.append(") ON COLUMNS FROM ");
-		builder.append(cube.getUniqueName());
+        builder.append(") ON COLUMNS FROM ");
+        builder.append(cube.getUniqueName());
 
-		Exp slicer = query.getParsedQuery().getSlicer();
+        Exp slicer = query.getParsedQuery().getSlicer();
 
-		if (slicer != null) {
-			builder.append(" WHERE ");
-			builder.append(slicer.toMdx());
-		}
+        if (slicer != null) {
+            builder.append(" WHERE ");
+            builder.append(slicer.toMdx());
+        }
 
-		List<MetadataElement> members;
-		if (selection == null) {
-			members = Collections.emptyList();
-		} else {
-			members = new LinkedList<MetadataElement>();
+        List<MetadataElement> members;
+        if (selection == null) {
+            members = Collections.emptyList();
+        } else {
+            members = new LinkedList<MetadataElement>();
 
-			for (MetadataElement elem : selection) {
-				if (elem instanceof Member){
-					members.add(utils.wrapRaggedIfNecessary((Member) elem));
-				} else if (elem instanceof Level) {
-					members.add(elem);
-				}
-			}
-		}
+            for (MetadataElement elem : selection) {
+                if (elem instanceof Member) {
+                    members.add(utils.wrapRaggedIfNecessary((Member) elem));
+                } else if (elem instanceof Level) {
+                    members.add(elem);
+                }
+            }
+        }
 
-		if (!members.isEmpty()) {
-			builder.append(" RETURN ");
+        if (!members.isEmpty()) {
+            builder.append(" RETURN ");
 
-			isFirst = true;
+            isFirst = true;
 
-			for (MetadataElement elem : members) {
-				if (isFirst) {
-					isFirst = false;
-				} else {
-					builder.append(", ");
-				}
+            for (MetadataElement elem : members) {
+                if (isFirst) {
+                    isFirst = false;
+                } else {
+                    builder.append(", ");
+                }
 
-				builder.append(elem.getUniqueName());
-			}
-		}
+                builder.append(elem.getUniqueName());
+            }
+        }
 
-		String mdx = builder.toString();
+        String mdx = builder.toString();
 
-		if (logger.isDebugEnabled()) {
-			logger.debug("Drill through MDX : {}", mdx);
-		}
+        if (logger.isDebugEnabled()) {
+            logger.debug("Drill through MDX : {}", mdx);
+        }
 
-		ResultSet result;
+        ResultSet result;
 
-		try {
-			Statement stmt = getConnection().createStatement();
-			result = stmt.executeQuery(mdx);
-		} catch (SQLException e) {
-			throw new PivotException(e);
-		}
+        try {
+            Statement stmt = getConnection().createStatement();
+            result = stmt.executeQuery(mdx);
+        } catch (SQLException e) {
+            throw new PivotException(e);
+        }
 
-		return result;
-	}
+        return result;
+    }
 }
