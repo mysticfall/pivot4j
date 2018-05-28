900b957c787776dd70fab7e0c9bcd93d8418d1ed
Judilson José da Costa Júnior <judilsonjunior@gmail.com>
2018-05-28 17:22:39 -0300

    No saved view state could be found for the view identi… By mcastillob

---
+++ pivot4j-analytics/src/main/java/org/pivot4j/analytics/exception/ViewExpiredExceptionHandler.java
@@ -0,0 +1,50 @@
+/*
+ * ====================================================================
+ * This software is subject to the terms of the Common Public License
+ * Agreement, available at the following URL:
+ *   http://www.opensource.org/licenses/cpl.html .
+ * You must accept the terms of that agreement to use this software.
+ * ====================================================================
+ */
+package org.pivot4j.analytics.exception;
+
+import java.util.Iterator;
+
+import javax.faces.FacesException;
+import javax.faces.application.ViewExpiredException;
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerWrapper;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ExceptionQueuedEvent;
+
+public class ViewExpiredExceptionHandler extends ExceptionHandlerWrapper {
+
+    private ExceptionHandler wrapped;
+
+    public ViewExpiredExceptionHandler(ExceptionHandler wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public void handle() throws FacesException {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        for (Iterator<ExceptionQueuedEvent> iter = getUnhandledExceptionQueuedEvents().iterator(); iter.hasNext();) {
+            Throwable exception = iter.next().getContext().getException();
+
+            if (exception instanceof ViewExpiredException) {
+                facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, "index");
+                facesContext.renderResponse();
+                iter.remove();
+            }
+        }
+
+        getWrapped().handle();
+    }
+
+    @Override
+    public ExceptionHandler getWrapped() {
+        return wrapped;
+    }
+
+}
