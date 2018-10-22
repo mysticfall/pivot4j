9f460af2231b0a15c36b75ec25653b7bcb0fe5b6
Judilson <Judilson@DESKTOP-M5PDL85>
2018-10-21 23:32:52 -0200

    Login and logout

---
+++ pivot4j-analytics/src/main/java/org/pivot4j/analytics/security/SecurityWebConfig.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pivot4j.analytics.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *
+ * @author Judilson
+ */
+@EnableWebSecurity
+public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+
+        // in-memory authentication
+        BCryptPasswordEncoder encoder = passwordEncoder();
+        auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("password")).roles("USER");
+        // using custom UserDetailsService DAO
+        // auth.userDetailsService(new AppUserDetailsServiceDAO());
+        // using JDBC
+//        Context ctx = new InitialContext();
+//        DataSource ds = (DataSource) ctx
+//                .lookup("java:/comp/env/jdbc/MyLocalDB");
+//
+//        final String findUserQuery = "select username,password,enabled "
+//                + "from Employees " + "where username = ?";
+//        final String findRoles = "select username,role " + "from Roles "
+//                + "where username = ?";
+//
+//        auth.jdbcAuthentication().dataSource(ds)
+//                .usersByUsernameQuery(findUserQuery)
+//                .authoritiesByUsernameQuery(findRoles);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                // ... per H2 console
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                //
+                .authorizeRequests()
+                .antMatchers("/", "/home","/register","/input","/output","/output/*","/**/*.css","/**/*.js","/**/*.png","/**/*.svg").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/security/login.xhtml").permitAll()
+                .failureUrl("/security/login.xhtml?error=true")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/security/login.xhtml");
+//        http
+//                //.loginPage("/login")
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .headers().disable()
+//                .csrf().disable()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/login");
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                
+//                .and()
+//                .headers().disable()
+//                .csrf().disable()
+//                .formLogin();
+    }
+}
